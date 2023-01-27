079d6c908eb46d594adb8254e788a68ab63c4732
Yehor Popovych <popovych.yegor@gmail.com>
2023-01-27 21:59:34 +0000

    fixed panic handler

--- rust/tesseract_utils/src/panic.rs
+++ rust/tesseract_utils/src/panic.rs
@@ -1,6 +1,5 @@
 use super::error::CError;
 use super::result::Result;
-use super::traits::IntoC;
 use std::any::Any;
 use std::panic;
 
@@ -18,17 +17,16 @@ pub fn handle_exception<F, R>(func: F) -> Result<R>
 where
     F: FnOnce() -> R + panic::UnwindSafe,
 {
-    handle_exception_result(|| Result::Ok(func()))
+    handle_exception_result(|| Ok(func()))
 }
 
-pub fn handle_exception_result<F, R, E>(func: F) -> Result<R>
+pub fn handle_exception_result<F, R>(func: F) -> Result<R>
 where
-    F: FnOnce() -> std::result::Result<R, E> + panic::UnwindSafe,
-    E: IntoC<CVal = CError>,
+    F: FnOnce() -> Result<R> + panic::UnwindSafe,
 {
     panic::catch_unwind(func)
         .map_err(|err| CError::Panic(string_from_panic_err(err).into()))
-        .and_then(|res| res.map_err(|e| e.into_c()))
+        .and_then(|res| res)
 }
 
 #[allow(dead_code)]
