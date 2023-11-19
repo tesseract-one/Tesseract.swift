6a85bf77d82b671052c3b4355cecffde9f3882f4
Yehor Popovych <popovych.yegor@gmail.com>
2023-11-19 16:43:54 +0000

    updated header generation script

--- Rust/generate_headers.sh
+++ Rust/generate_headers.sh
@@ -3,20 +3,19 @@ set -e
 
 MODULE_NAME="CTesseractShared"
 MAIN_CRATE="tesseract-swift-transports"
-HEADERS=("tesseract-swift-utils.h" "tesseract-swift-transports.h")
 HEADERS_DIR="target/release/include"
 OUTPUT_DIR="Sources/$MODULE_NAME/include"
 
 DIR="$(cd "$(dirname "$0")" && pwd -P)/.."
 cd "$DIR"
 
+rm -rf "$HEADERS_DIR"
+
 cargo build -p "$MAIN_CRATE" --release --all-features
 
 rm -rf "$OUTPUT_DIR"
 mkdir -p "$OUTPUT_DIR"
 
-for header in "${HEADERS[@]}"; do
-  cp "$HEADERS_DIR/$header" "$OUTPUT_DIR/"
-done
+cp "$HEADERS_DIR/"*.h "$OUTPUT_DIR/"
 
 exit 0
